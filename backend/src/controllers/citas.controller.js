4f8a435121bd15143dff6fba3e60429ed9bed69c
takashimonn7 <takashimoon7@gmail.com>
2024-08-27 20:46:49 -0600

    endoints para citas termiandas

--- backend/src/controllers/citas.controller.js
+++ backend/src/controllers/citas.controller.js
@@ -16,17 +16,37 @@ export const GetCita = async(req, res) => {
 
 export const CreateCita = async(req, res) => {
     const connection = await connect();
-    const [results]= await connection.query("INSERT INTO citas(nombre, apellido, especialidad, correo_electronico, telefono, usuario, contrasena) VALUES (?,?,?,?,?,?,?)", [
-        req.body.nombre,
-        req.body.apellido,
-        req.body.especialidad,
-        req.body.correo_electronico,
-        req.body.telefono,
-        req.body.usuario,
-        req.body.contrasena,
+    const [results]= await connection.query("INSERT INTO citas(id_psicologo, id_paciente, fecha_cita, hora_cita, tipo_cita, estado) VALUES (?,?,?,?,?,?)", [
+        req.body.id_psicologo,
+        req.body.id_paciente,
+        req.body.fecha_cita,
+        req.body.hora_cita,
+        req.body.tipo_cita,
+        req.body.estado,
     ]);
     res.json({
         id: results.insertId,
         ...req.body,
     });
+}
+
+export const DeleteCita = async(req, res) => {
+    const connection = await connect();
+    await connection.query("DELETE FROM citas WHERE id_cita = ?", [
+        req.params.id_cita,
+    ]);
+    res.sendStatus(204);
+}
+
+export const PutCita = async(req, res) => {
+    const connection = await connect();
+    await connection.query("UPDATE citas SET ? WHERE id_cita = ?", [
+        req.body,
+        req.params.id_cita,
+    ]);
+    const [rows] = await connection.query(
+        "SELECT * FROM citas WHERE id_cita = ?",
+        [req.params.id_cita]
+    );
+    res.json(rows[0]);
 }
