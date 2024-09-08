a58a09ac21cd313447bc79fc901f86ac30ec26d4
takashimonn7 <takashimoon7@gmail.com>
2024-09-08 13:40:29 -0600

    endpoint de diagnostico terminado

--- backend/src/controllers/diagnosticos.controller.js
+++ backend/src/controllers/diagnosticos.controller.js
@@ -5,3 +5,46 @@ export const GetDiagnosticos = async(req, res) => {
     const [rows] = await connection.query('SELECT * FROM diagnosticos');
     res.json(rows);
 }
+
+export const GetDiagnostico = async(req, res) => {
+    const connection = await connect();
+    const [rows] = await connection.query("SELECT * FROM diagnosticos WHERE id_diagnostico = ?", [
+        req.params.id_diagnostico,
+    ]);
+    res.json(rows[0]);
+}
+
+export const CreateDiagnostico = async(req, res) => {
+    const connection = await connect();
+    const [results]= await connection.query("INSERT INTO diagnosticos(id_paciente, id_psicologo, fecha_diagnostico, diagnostico) VALUES (?,?,?,?)", [
+        req.body.id_paciente,
+        req.body.id_psicologo,
+        req.body.fecha_diagnostico,
+        req.body.diagnostico,
+    ]);
+    res.json({
+        id: results.insertId,
+        ...req.body,
+    });
+}
+
+export const DeleteDiagnostico = async(req, res) => {
+    const connection = await connect();
+    await connection.query("DELETE FROM diagnosticos WHERE id_diagnostico = ?", [
+        req.params.id_diagnostico,
+    ]);
+    res.sendStatus(204);
+}
+
+export const PutDiagnostico = async(req, res) => {
+    const connection = await connect();
+    await connection.query("UPDATE diagnosticos SET ? WHERE id_diagnostico = ?", [
+        req.body,
+        req.params.id_diagnostico,
+    ]);
+    const [rows] = await connection.query(
+        "SELECT * FROM diagnosticos WHERE id_diagnostico = ?",
+        [req.params.id_diagnostico]
+    );
+    res.json(rows[0]);
+}
--- backend/src/routes/diagnosticos.router.js
+++ backend/src/routes/diagnosticos.router.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
-import { GetDiagnosticos } from '../controllers/diagnosticos.controller'
+import { GetDiagnosticos, GetDiagnostico, CreateDiagnostico, DeleteDiagnostico, PutDiagnostico } from '../controllers/diagnosticos.controller'
 
 
 const router = Router();
 
 router.get('/diagnosticos', GetDiagnosticos);
-
+router.get('/diagnosticos/:id_diagnostico', GetDiagnostico);
+router.post('/diagnosticos', CreateDiagnostico);
+router.delete('/diagnosticos/:id_diagnostico', DeleteDiagnostico)
+router.put('/diagnosticos/:id_diagnostico', PutDiagnostico);
 
 export default router 
