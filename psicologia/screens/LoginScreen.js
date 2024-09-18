747fc14330af69fcdcbbe562b3b749d2711813e7
alondracarrillocelis <alondra_3127200055@utd.edu.mx>
2024-09-18 15:45:06 -0600

    add design to login

--- psicologia/screens/LoginScreen.js
+++ psicologia/screens/LoginScreen.js
@@ -2,11 +2,13 @@ import { View, Text, TouchableOpacity, StyleSheet, TextInput, Alert } from 'reac
 import React, { useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Image } from 'expo-image'; // Cambiamos a Image
 
 const LoginScreen = () => {
   const navigation = useNavigation();
   const [usuario, setUsuario] = useState('');
   const [contrasena, setContrasena] = useState('');
+  const [rol, setRol] = useState('psicologo'); // Estado para el rol
 
   const handleLogin = async () => {
     if (!usuario || !contrasena) {
@@ -14,13 +16,18 @@ const LoginScreen = () => {
       return;
     }
 
+    if (!rol) {
+      Alert.alert('Error', 'Por favor, selecciona el rol.');
+      return;
+    }
+
     try {
-      const response = await fetch('http://192.168.1.16:3000/login', {
+      const response = await fetch('http://172.20.106.61:3000/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ usuario, contrasena }),
+        body: JSON.stringify({ usuario, contrasena, rol }), // Enviamos también el rol
       });
 
       if (!response.ok) {
@@ -35,7 +42,7 @@ const LoginScreen = () => {
 
       if (data.exists && data.user) {
         // Guarda el rol y otros datos en AsyncStorage
-        const { rol, id_psicologo } = data.user; // Accede a los datos anidados
+        const { rol, id_psicologo, id_paciente } = data.user; // Accede a los datos anidados
 
         if (rol) {
           await AsyncStorage.setItem('userRole', rol.toString());
@@ -45,6 +52,8 @@ const LoginScreen = () => {
 
         if (id_psicologo) {
           await AsyncStorage.setItem('userId', id_psicologo.toString());
+        } else if (id_paciente) {
+          await AsyncStorage.setItem('userId', id_paciente.toString());
         } else {
           console.warn('El ID del usuario no está definido en la respuesta.');
         }
@@ -65,12 +74,20 @@ const LoginScreen = () => {
 
   return (
     <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={require('../assets/logo.png')} // Imagen en la parte superior
+      />
+         <Text style={styles.title}>Bienvenido/a</Text>
+         <Text style={styles.subtitle}>Ingresa tus datos</Text>
       <TextInput 
         placeholder="Usuario" 
         style={styles.input}
         onChangeText={text => setUsuario(text)}
         value={usuario}
       />
+
+   
       
       <TextInput 
         placeholder="Contraseña" 
@@ -80,7 +97,23 @@ const LoginScreen = () => {
         value={contrasena}
       />
 
-      <TouchableOpacity onPress={handleLogin}>
+      <View style={styles.roleContainer}>
+        <TouchableOpacity 
+          onPress={() => setRol('psicologo')} 
+          style={[styles.roleButton, rol === 'psicologo' && styles.selectedRole]}
+        >
+          <Text style={styles.buttonText}>Psicólogo</Text>
+        </TouchableOpacity>
+        
+        <TouchableOpacity 
+          onPress={() => setRol('paciente')} 
+          style={[styles.roleButton, rol === 'paciente' && styles.selectedRole]}
+        >
+          <Text style={styles.buttonText}>Paciente</Text>
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
         <Text style={styles.buttonText}>Continuar</Text>
       </TouchableOpacity>
     </View>
@@ -93,21 +126,68 @@ const styles = StyleSheet.create({
     justifyContent: 'center', 
     alignItems: 'center', 
     padding: 20,
+    backgroundColor: '#b8b6f2', // Fondo color lila
+  },
+  image: {
+    width: "30%",
+    height: "30%",
+    marginBottom: 10, // Espacio entre la imagen y el formulario
+  },
+  title: {
+    fontSize: 30,
+    color: '#FFFFFF',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    marginBottom: 15,
+  
+
+  }
+  ,
+  subtitle: {
+    fontSize: 20,
+    color: '#FFFFFF',
+    textAlign: 'center',
+    marginBottom: 30,
+  
+    
+
   },
   input: {
     width: '80%', 
     height: 40, 
     borderColor: '#ccc', 
     borderWidth: 1, 
-    borderRadius: 5, 
+    borderRadius: 19, 
     paddingLeft: 10, 
     marginBottom: 20,
+    backgroundColor: '#FFF', 
+    color:"#ABA6A6"
   },
   buttonText: {
     fontSize: 18,
-    color: 'blue',
+    color: '#FFFFFF',
     textAlign: 'center',
+  },
+  roleContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 20,
+  },
+  roleButton: {
+    padding: 10,
+    backgroundColor: '#918FCC',
+    borderRadius: 19,
+    marginHorizontal: 5,
+  },
+  selectedRole: {
+    backgroundColor: '#6A5ACD', // Color diferente para el botón seleccionado
+  },
+  loginButton: {
+    padding: 10,
+    backgroundColor: '#918FCC',
+    borderRadius: 19,
     marginTop: 20,
+    width: '80%',
   },
 });
 
