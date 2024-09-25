1293923af9852b13dd7ec70ba27caa8c876f2fb0
takashimonn7 <takashimoon7@gmail.com>
2024-09-25 01:05:53 -0600

    restauracion app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,12 @@
 import express from 'express'; 
 import cors from 'cors';
 import morgan from 'morgan'; 
-// import PsicologosRouter from './routes/psicologos.router';
-import PsicologosRouter from './routes/psicologos.router.js';
-import pacientesRouter from './routes/pacientes.router.js';
-import CitasRouter from './routes/citas.router.js';
-import DiagnosticosRouter from './routes/diagnosticos.router.js';
-import NotasRouter from './routes/notas.router.js';
+
+import PsicologosRouter from './routes/psicologos.router';
+import pacientesRouter from './routes/pacientes.router';
+import CitasRouter from './routes/citas.router';
+import DiagnosticosRouter from './routes/diagnosticos.router';
+import NotasRouter from './routes/notas.router';
 
 const app = express();
 
@@ -15,14 +15,14 @@ app.use(morgan());
 app.use(express.json());
 
 
-import loginRouter from './routes/login.router.js'; 
+import loginRouter from './routes/login.router'; 
 app.use(loginRouter);
 
 app.use(loginRouter);
 app.use(PsicologosRouter);
 app.use(CitasRouter);
-app.use( pacientesRouter);
+app.use(pacientesRouter);
 app.use(DiagnosticosRouter);
 app.use(NotasRouter);
 
-export default app; 
+export default app;
