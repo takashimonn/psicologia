bfddaa81d1e80232bd5fa2c90b0f5bd04b8136fb
takashimonn7 <takashimoon7@gmail.com>
2024-09-24 20:31:20 -0600

    ojala que no truene esta madre

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,31 @@
-import app from './app';
+import express from 'express'; 
+import cors from 'cors';
+import morgan from 'morgan'; 
+import PsicologosRouter from './routes/psicologos.router';
+import pacientesRouter from './routes/pacientes.router';
+import CitasRouter from './routes/citas.router';
+import DiagnosticosRouter from './routes/diagnosticos.router';
+import NotasRouter from './routes/notas.router';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(cors());
+app.use(morgan());
+app.use(express.json());
+
+
+import loginRouter from './routes/login.router'; 
+app.use(loginRouter);
+
+app.use(PsicologosRouter);
+app.use(CitasRouter);
+app.use(pacientesRouter);
+app.use(DiagnosticosRouter);
+app.use(NotasRouter);
+
+// import app from './app';
 
 app.listen(3000)
 console.log('server on port 3000')
+export default app; 
---
+++ backend/src/index2.js
@@ -0,0 +1,4 @@
+import app from '.';
+
+app.listen(3000)
+console.log('server on port 3000')
